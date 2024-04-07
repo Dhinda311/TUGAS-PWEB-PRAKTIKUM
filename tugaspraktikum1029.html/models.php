43293c9095be158b1a6fb6087a27808873b7264c
dhinda_ <67671568+Dhinda311@users.noreply.github.com>
2024-04-07 21:33:22 +0700

    Add files via upload

---
+++ tugaspraktikum1029.html/models.php
@@ -0,0 +1,20 @@
+<?php
+class Contacts {
+    //SELECT
+    public static function select() {  
+    }
+    //INSERT
+    public static function insert() {
+    }
+    //UPDATE
+    public static function update() {
+    }
+    //DELETE
+    public static function delete() {
+    }
+    
+    public $id_akun;
+    public $no_hp;
+    public $owner;
+}
+?>
